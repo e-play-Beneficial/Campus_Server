63f1dd30227a9b002ba4bab4a85d1f0ae79d43b5
Yongbin <rhdydqls1024@naver.com>
2022-05-24 10:06:58 +0900

    ⚙ : Setting

---
+++ src/test/java/com/project/Campus/CampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Campus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
